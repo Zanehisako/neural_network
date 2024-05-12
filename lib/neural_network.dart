5d61c70276977e5463014384c586229f0d8f3a8b
Zanehisako <yassinetakiko@gmail.com>
2024-05-12 20:11:21 +0100

    Finished An early version of the job recommendation AI

--- lib/neural_network.dart
+++ lib/neural_network.dart
@@ -13,14 +13,16 @@ class NeuralNetwork {
   double error = 0.0;
   double averageError = 0.0;
   NeuralNetwork(
-      int numOfneurons1,
-      int numOfBiases1,
-      int numOfneurons2,
-      int numOfBiases2,
-      int numOfneurons3,
-      int numOfBiases3,
-      double eta,
-      double alpha) {
+    int numOfneurons1,
+    int numOfBiases1,
+    int numOfneurons2,
+    int numOfBiases2,
+    int numOfneurons3,
+    int numOfBiases3,
+    double eta,
+    double alpha,
+    ActivationFunctions activationFunctions,
+  ) {
     layers.addAll([
       NeuralLayer(numOfneurons1, numOfBiases1, numOfneurons2, eta, alpha),
       NeuralLayer(numOfneurons2, numOfBiases2, numOfneurons3, eta, alpha),
@@ -68,7 +70,7 @@ class NeuralNetwork {
     ]);
   }
 
-  void PrintNetwork(int datalength) {
+  void PrintNetwork() {
     layers.forEach((element) {
       print('---------------------------');
       print(element.neurons.length);
@@ -83,8 +85,6 @@ class NeuralNetwork {
         print("the gradiant is : ${element.gradiant}");
       });
     });
-    print(
-        'The average Error of the network was : ${averageError / datalength}');
     print('---------------------------');
   }
 
@@ -157,6 +157,27 @@ class NeuralNetwork {
     }
     averageError += networkError;
   }
+
+  void Train(List<Data> data) {
+    for (var j = 0; j < data.length; j++) {
+      FeedFoward(data[j].inputs);
+      BackPropagitation([data[j].targetValue]);
+      print('');
+      print('the inputs are : ${data[j].inputs.first} ${data[j].inputs.last}');
+      print('the target value is : ${data[j].targetValue}');
+      GetResults();
+      if (j == data.length - 1) {
+        print('the network while training');
+        PrintNetwork();
+      }
+    }
+    print('Finished Training');
+  }
+
+  double Use(List<double> inputs) {
+    FeedFoward(inputs);
+    return layers.last.neurons[0].myValue;
+  }
 }
 
 class NeuralNetworkUi extends StatefulWidget {
@@ -170,31 +191,57 @@ class _NeuralNetworkUiState extends State<NeuralNetworkUi> {
   TextEditingController distanceController = TextEditingController();
   TextEditingController xController = TextEditingController();
   TextEditingController yController = TextEditingController();
-  NeuralNetwork network = NeuralNetwork(2, 0, 3, 3, 1, 1, 0.1, 0.1);
+  NeuralNetwork xornetwork =
+      NeuralNetwork(2, 0, 3, 3, 1, 1, 0.1, 0.1, ActivationFunctions.tanh);
+  NeuralNetwork jobnetwork =
+      NeuralNetwork(2, 0, 3, 3, 1, 1, 0.1, 0.5, ActivationFunctions.sigmoid);
   File dataFile = File('Data.txt');
   File xorDataFile = File('Xor_Dataset.txt');
-  List<Data> data = [];
+  List<Data> xordata = [];
+  List<Data> jobdata = [];
   Trainer? trainer;
-  bool gettingResult = true;
-  double? result;
-  List<String> contents = [];
+  bool gettingXorResult = true;
+  bool gettingJobResult = true;
+  double? xorResult;
+  double? jobResult;
+  List<String> xorcontents = [];
+  List<String> jobContents = [];
 
   @override
   void initState() {
     setState(() {
-      contents = xorDataFile.readAsLinesSync();
+      xorcontents = xorDataFile.readAsLinesSync();
+      jobContents = dataFile.readAsLinesSync();
     });
-    ParseDataXOR(contents, data);
-    //ParseData(contents, data);
-    //network.PrintNetwork(data.length);
-    for (var i = 0; i < data.length; i++) {
-      network.FeedFoward(data[i].inputs);
-      network.BackPropagitation([data[i].targetValue]);
+    ParseDataXOR(xorcontents, xordata);
+    ParseData(jobContents, jobdata);
+    //train XOR network
+    /*for (var i = 0; i < xordata.length; i++) {
+      xornetwork.FeedFoward(xordata[i].inputs);
+      xornetwork.BackPropagitation([xordata[i].targetValue]);
       print('');
-      print('the inputs are : ${data[i].inputs.first} ${data[i].inputs.last}');
-      print('the target value is : ${data[i].targetValue}');
-      network.GetResults();
-    }
+      print(
+          'the inputs are : ${xordata[i].inputs.first} ${xordata[i].inputs.last}');
+      print('the target value is : ${xordata[i].targetValue}');
+      xornetwork.GetResults();
+    }*/
+    //train Job Network
+
+    jobnetwork.Train(jobdata);
+
+    //XORnetwork(1, 1);
+    jobnetwork.PrintNetwork();
+    JobNetwork(10000, 1);
+    JobNetwork(1, 100000);
+    //1
+    /*jobnetwork.GetResults();
+    print('3.0, 9.0');
+    jobnetwork.FeedFoward([3.0, 9.0]); //0.009
+    jobnetwork.GetResults();
+    print('600.0, 4.0');
+    jobnetwork.FeedFoward([600.0, 4.0]); //1
+    jobnetwork.GetResults();*/
+
     /*print('Before feedFoward');
     network.PrintNetwork(data.length);
     print('the inputs are :');
@@ -211,22 +258,28 @@ class _NeuralNetworkUiState extends State<NeuralNetworkUi> {
     super.initState();
   }
 
-  void NetworkResult(double pay, double distance) {
+  void JobNetwork(double pay, double distance) {
     print('the inputs are ${pay} ${distance}');
-    network.FeedFoward([pay, distance]);
+    jobnetwork.FeedFoward([pay, distance]);
     setState(() {
-      result = network.layers.last.neurons[0].myValue;
-      gettingResult = false;
+      jobResult = jobnetwork.layers.last.neurons[0].myValue;
+      gettingJobResult = false;
     });
+    print(jobResult);
   }
 
   void XORnetwork(double x, double y) {
     print('the inputs are ${x} ${y}');
-    network.FeedFoward([x, y]);
+    xornetwork.FeedFoward([x, y]);
     setState(() {
-      result = network.layers.last.neurons[0].myValue;
-      gettingResult = false;
+      if (xornetwork.layers.last.neurons[0].myValue > 0.99) {
+        xorResult = 1;
+      } else {
+        xorResult = 0;
+      }
+      gettingXorResult = false;
     });
+    print(xorResult);
   }
 
   @override
@@ -247,6 +300,22 @@ class _NeuralNetworkUiState extends State<NeuralNetworkUi> {
               controller: distanceController,
               decoration: const InputDecoration(hintText: 'distance'),
             ),
+            TextButton(
+                onPressed: () {
+                  double pay = double.parse(payController.text);
+                  double distance = double.parse(distanceController.text);
+                  setState(() {
+                    jobnetwork.Use([pay, distance]) > 0.5
+                        ? jobResult = 1
+                        : jobResult = 0;
+                  });
+                },
+                child: const Text('Start')),
+            gettingJobResult
+                ? const SizedBox()
+                : jobResult == 1
+                    ? const Text('Good Job')
+                    : const Text('Bad Job'),
             const SizedBox(
               height: 15,
             ),
@@ -267,12 +336,10 @@ class _NeuralNetworkUiState extends State<NeuralNetworkUi> {
                   double x = double.parse(xController.text);
                   double y = double.parse(yController.text);
                   XORnetwork(x, y);
-                  network.PrintNetwork(jobs.length);
-
-                  print(data.length);
+                  xornetwork.PrintNetwork();
                 },
                 child: const Text('Start')),
-            gettingResult ? SizedBox() : Text('$result'),
+            gettingXorResult ? const SizedBox() : Text('$xorResult'),
           ],
         ),
       )),
